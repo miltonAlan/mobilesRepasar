79cf8965c31cb5a5814cab90ccd39bef14da125f
mpaucar <miltonpaucar99@gmail.com>
2024-07-25 12:15:55 -0500

    agrego modelo de IA funcional

--- backend/src/lang_script.ts
+++ backend/src/lang_script.ts
@@ -1,13 +1,16 @@
-import {OpenAI} from "langchain";
-import {RetrievalQAChain} from "langchain/chains";
-import {PDFLoader} from "langchain/document_loaders/fs/pdf";
-import {OpenAIEmbeddings} from "langchain/embeddings/openai";
-import {MemoryVectorStore} from "langchain/vectorstores/memory";
+import { OpenAI } from "langchain";
+import { RetrievalQAChain } from "langchain/chains";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 
 
 export const process_doc = async (filename: string | undefined, question: string) => {
-    const model = new OpenAI({});
-    const loader = new PDFLoader(`/Users/pablocaiza/Documents/mobil-2/backend/uploads/${filename}`, {
+    const model = new OpenAI({
+        modelName: "gpt-4o", // Piso modelo anterior, estaba deprecado
+        temperature: 0.7,
+    });
+    const loader = new PDFLoader(`/Users/mpaucar/Desktop/mobilesRepasar/backend/uploads/${filename}`, {
         splitPages: false
     })
     const doc = await loader.load()
